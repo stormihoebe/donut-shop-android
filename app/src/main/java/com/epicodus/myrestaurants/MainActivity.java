b05a54f9a95c363caf1f72c97735291b69504dea
Christian Martinez and Shane Stafford <christian+shane@>
2017-05-22 11:35:14 -0700

    add zip input

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -3,22 +3,29 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private EditText mZipEditText;
     private Button mEmberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mZipEditText = (EditText) findViewById(R.id.enterZip);
         mEmberButton = (Button) findViewById(R.id.emberButton);
             mEmberButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    String zip = mZipEditText.getText().toString();
+                    Log.d(TAG, zip);
                     Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
                     startActivity(intent);
                 }
