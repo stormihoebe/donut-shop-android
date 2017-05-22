06afb5f24bec2ddd0477d1ae6e618168a9ab80a0
Christian Martinez and Shane Stafford <christian@>
2017-05-22 15:33:01 -0700

    add and pass espresso test for user input appearing on next action

---
+++ app/src/androidTest/java/com/epicodus/myrestaurants/MainActivityInstrumentationTest.java
@@ -0,0 +1,33 @@
+package com.epicodus.myrestaurants;
+
+import android.support.test.rule.ActivityTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void validateEdutText() {
+        onView(withId(R.id.enterZip)).perform(typeText("97214"))
+                .check(matches(withText("97214")));
+    }
+
+    @Test
+    public void locationIsSentToRestaurantsActivity() {
+        String zip = "97214";
+        onView(withId(R.id.enterZip)).perform(typeText(zip));
+        onView(withId(R.id.emberButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Yo, dawg. Here are all the donuts near "  + zip)));
+    }
+}
