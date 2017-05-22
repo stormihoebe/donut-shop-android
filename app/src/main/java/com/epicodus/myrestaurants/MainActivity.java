562949ad2f4c50d3f10ebeb59f49ee656a574868
Christian Martinez and Shane Stafford <christian+shane@>
2017-05-22 11:00:01 -0700

    add toast function to button

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -2,12 +2,24 @@ package com.epicodus.myrestaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mEmberButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEmberButton = (Button) findViewById(R.id.emberButton);
+            mEmberButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "It's not that bad. We just like the meme", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
